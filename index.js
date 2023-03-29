c330424d6e380ed50b4b7f442d96cb70bea0cb21
Ashik Hussain <muhammedashikth@gmail.com>
2023-03-29 17:08:51 +0530

    change socket confg to serverjs

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
-var cors = require('cors')
-app.use(cors())
+const cors = require('cors')
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
 const postRoute = require('./routes/posts')
@@ -15,9 +14,61 @@ const messageRoute = require('./routes/messages')
 const conversationRoute = require('./routes/conversations')
 
 
+// Socket.io configuration
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+const httpServer = createServer(app)
 
-app.use('/specImage',express.static('Spec-Image'))
+const io = new Server(httpServer,{
+    cors:{
+        origin:"https://main.d1u4ylaq20kul3.amplifyapp.com/"
+    }
+});
 
+let users = [];
+
+const addUser = (userId,socketId) =>{
+    !users.some(user =>user.userId === userId) && 
+    users.push({userId,socketId})
+}
+
+const removeUser = (socketId) =>{
+    users = users.filter((user)=>user.socketId !== socketId)
+}
+
+const getUser = (userId) =>{
+    return users.find((user=>user.userId === userId))
+}
+
+io.on("connection",(socket)=>{
+    // when connect
+    console.log("a user connected")  
+
+    //take userid and socketid from user
+    socket.on("addUser",id =>{
+        addUser(id,socket.id)
+        io.emit("getUsers",users)  
+    })
+
+    //send and get message
+    socket.on("sendMessage",({senderId,receiverId,text}) =>{
+        console.log(senderId,receiverId,text)
+
+        const user = getUser(receiverId);
+
+        io.to(user?.socketId).emit("getMessage",{ 
+            senderId,text
+        })
+    })
+
+    //when disconnect
+    socket.on("disconnect",()=> {
+        console.log("a user disconnected")
+        removeUser(socket.id)
+    })
+})
+
+// socket io configuration ends
 
 
 dotenv.config()
@@ -25,12 +76,12 @@ mongoose.connect('mongodb://localhost:27017/feelgood',()=>{
     console.log("Connected to MongoDB")
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-
-  
+app.use('/specImage',express.static('Spec-Image'))
 app.use('/api/users',userRoute)
 app.use('/api/auth',authRoute)
 app.use('/api/posts',postRoute)
