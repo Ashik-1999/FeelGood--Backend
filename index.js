36f84daa2075911dda78594097f145313f151234
Ashik Hussain <muhammedashikth@gmail.com>
2023-03-30 22:31:31 +0530

    update cors

--- index.js
+++ index.js
@@ -17,58 +17,67 @@ const { createServer } = require('http')
 
 
 // Socket.io configuration
-const httpServer = createServer(app)
+// const httpServer = createServer(app)
 
-const io = new Server(httpServer,{
-    cors:{
-        origin:"https://main.d1u4ylaq20kul3.amplifyapp.com"
-    }
-});
+// const io = new Server(httpServer,{
+//     cors:{
+//         origin:"https://main.d1u4ylaq20kul3.amplifyapp.com"
+//     }
+// });
 
-let users = [];
+// let users = [];
 
-const addUser = (userId,socketId) =>{
-    !users.some(user =>user.userId === userId) && 
-    users.push({userId,socketId})
-}
+// const addUser = (userId,socketId) =>{
+//     !users.some(user =>user.userId === userId) && 
+//     users.push({userId,socketId})
+// }
 
-const removeUser = (socketId) =>{
-    users = users.filter((user)=>user.socketId !== socketId)
-}
+// const removeUser = (socketId) =>{
+//     users = users.filter((user)=>user.socketId !== socketId)
+// }
 
-const getUser = (userId) =>{
-    return users.find((user=>user.userId === userId))
-}
+// const getUser = (userId) =>{
+//     return users.find((user=>user.userId === userId))
+// }
 
-io.on("connection",(socket)=>{
-    // when connect
-    console.log("a user connected")  
+// io.on("connection",(socket)=>{
+//     // when connect
+//     console.log("a user connected")  
 
-    //take userid and socketid from user
-    socket.on("addUser",id =>{
-        addUser(id,socket.id)
-        io.emit("getUsers",users)  
-    })
+//     //take userid and socketid from user
+//     socket.on("addUser",id =>{
+//         addUser(id,socket.id)
+//         io.emit("getUsers",users)  
+//     })
 
-    //send and get message
-    socket.on("sendMessage",({senderId,receiverId,text}) =>{
-        console.log(senderId,receiverId,text)
+//     //send and get message
+//     socket.on("sendMessage",({senderId,receiverId,text}) =>{
+//         console.log(senderId,receiverId,text)
 
-        const user = getUser(receiverId);
+//         const user = getUser(receiverId);
 
-        io.to(user?.socketId).emit("getMessage",{ 
-            senderId,text
-        })
-    })
+//         io.to(user?.socketId).emit("getMessage",{ 
+//             senderId,text
+//         })
+//     })
 
-    //when disconnect
-    socket.on("disconnect",()=> {
-        console.log("a user disconnected")
-        removeUser(socket.id)
-    })
-})
+//     //when disconnect
+//     socket.on("disconnect",()=> {
+//         console.log("a user disconnected")
+//         removeUser(socket.id)
+//     })
+// })
 
 // socket io configuration ends
+
+app.use(cors({
+    origin: 'https://main.d1u4ylaq20kul3.amplifyapp.com/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+    credentials: true,
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }))
 
 
 dotenv.config()
@@ -100,6 +109,6 @@ app.use('/messages',messageRoute)
 //     res.send("<h1>welcome to homepage</h1 >")
 // })
 
-httpServer.listen(3000,() => {
+app.listen(3000,() => {
     console.log("server is running on port 8080")
 })
